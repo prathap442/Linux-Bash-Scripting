a2b697c9b96cbd710df8c7a7e63d363e2a0e8e18
prathap442 <pmohansaikrishna@gmail.com>
2019-07-31 22:43:12 +0530

    made changes like adding new files

---
+++ wild_cards/README.md
@@ -0,0 +1,16 @@
+```
+ls a*
+```
+  the command does the operation of all the files that start with the letter a
+
+
+```
+ls a*.sh
+```
+ the command does the operation of giving all the files that start with letter a and end with .sh
+
+
+```
+ $ ls *.txt
+```
+  the command above will give all the files that end with the .txt extension
---
+++ wild_cards/example1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "this gives all the files starting with the letter e"
+ls e*
+<<COMMENT
+  ls e* gives all the files that start with letter e
+COMMENT
+
+echo "this gives all the files starting with e and ending with *.txt extension"
+ls e*.sh
+<<COMMENT
+  ls e*.sh gives all the files that start with e and end with sh
+COMMENT
+
+
+echo "this gives all the files with *.txt extension"
+ls *.txt
+echo "this is equivalent to ls a.txt b.txt c.txt d.txt ab.txt"
+<<COMMENT
+  this gives all the files that end with a .txt extension
+COMMENT
+
+
+<<COMMENT
+  ? represents basically the single character
+  ls ? this command basically gives us all the single character files
+COMMENT
+
+#ls ?.txt
+<<COMMENT
+ ls ?.txt
+ ls ??.txt
+COMMENT
+
+ls ??.txt
